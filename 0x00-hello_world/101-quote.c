224bb68d463b1e0d21339c6022ce303aa4b8eae8
Precious <111221686+Idaiye@users.noreply.github.com>
2022-09-13 03:52:36 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
@@ -6,10 +7,10 @@
  *
  * Return : Always 1
  * \
-  int main(void)
- {
+
+int main(void)
+{
 
  write(2,and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
  return (1);
- }
- 
+}
